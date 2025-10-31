12c3bce721006f026a3beda98287f077fab7190c
이호영 <win_dy1207@naver.com>
2025-11-01 00:42:18 +0900

    commit 2025-11-01  0:42:18.36

--- auto.bat
+++ auto.bat
@@ -1,9 +1,12 @@
 @echo off
 
-REM 아래는 pwd해서 프로젝트 경로 적어야 해
-cd /d "C:\Users\user\Desktop\code"
+REM "C:\Users\user\Desktop\code"
+cd /d "%~dp0" 
+git init
 
-git pull origin main
+git remote add origin https://github.com/D2PANG/CBT
+
+git pull origin master
 
 if errorlevel 1 (
     echo pull 실패
@@ -14,14 +17,14 @@ if errorlevel 1 (
 git add .
 set DATE=%date%
 set TIME=%time%
-
-REM 혹시 모르니까 시간으로 커밋 메시지
 git commit -m "commit %DATE% %TIME%"
 
-git push origin main
+git push origin master 
 
 if errorlevel 1 (
     echo push 실패
     pause
     exit /b
 )
+
+pause
